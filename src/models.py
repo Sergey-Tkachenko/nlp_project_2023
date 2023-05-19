0f135bef688077cf68489c8d708f0db9ebc25e43
Sergey Tkachenko <tkachenko.sm@phystech.edu>
2023-05-20 00:53:09 +0300

    added mean max pooler

--- src/models.py
+++ src/models.py
@@ -17,7 +17,6 @@ class BaseClassificationHead(torch.nn.Module):
                 **inputs: dict[Any]):
         """
         Inputs have following shapes:
-            pooler_output: [BATCH_SIZE, HIDDEN_SIZE],
             last_hidden_state: [BATCH_SIZE, SEQ_LENGTH, HIDDEN_SIZE]
             hidden_states: [NUMBER_LAYERS, BATCH_SIZE, SEQ_LENGTH, HIDDEN_SIZE]
         
@@ -45,6 +44,22 @@ class DebertaV2WithCustomClassifier(torch.nn.Module):
         return self.deberta.device
     
 
+def build_perceptron(hidden_sizes: list[int]):
+    
+    classifier = torch.nn.Sequential()
+
+    for input_size, output_size in zip(hidden_sizes[:-2], hidden_sizes[1:-1]):
+        classifier.append(torch.nn.Linear(input_size, output_size))
+
+        classifier.append(torch.nn.BatchNorm1d(output_size))
+
+        classifier.append(torch.nn.LeakyReLU())
+
+    classifier.append(torch.nn.Linear(hidden_sizes[-2], hidden_sizes[-1]))
+
+    return classifier
+    
+
 class PerceptronPoooler(BaseClassificationHead):
     """
     Simple classifiecation head that uses only last hidden state of CLS token.
@@ -63,18 +78,65 @@ class PerceptronPoooler(BaseClassificationHead):
         """
         super().__init__()
 
-        self.classifier = torch.nn.Sequential()
-
-        for input_size, output_size in zip(hidden_sizes[:-2], hidden_sizes[1:-1]):
-            self.classifier.append(torch.nn.Linear(input_size, output_size))
-
-            self.classifier.append(torch.nn.BatchNorm1d(output_size))
-
-            self.classifier.append(torch.nn.ReLU())
-
-        self.classifier.append(torch.nn.Linear(hidden_sizes[-2], hidden_sizes[-1]))
+        self.classifier = build_perceptron(hidden_sizes)
 
     def forward(self, model_outputs: BaseModelOutput, **inputs: dict[Any, Any]):
         cls_token_hidden_state = model_outputs.last_hidden_state[:, 0]
 
         return self.classifier(cls_token_hidden_state)
+
+
+class MeanMaxPooler(BaseClassificationHead):
+    def __init__(self, input_hidden_size : int, size_after_pooling: int,
+                 output_perceptron_sizes: list[int]) -> None:
+        super().__init__()
+
+        
+        
+        self.down_size_linear_layers = torch.nn.ModuleDict()
+
+        for name in ["mean", "max"]:
+            self.down_size_linear_layers[name] = torch.nn.Sequential(
+                torch.nn.Linear(input_hidden_size, size_after_pooling),
+                torch.nn.BatchNorm1d(size_after_pooling),
+                torch.nn.LeakyReLU()
+            )
+
+        self.classifier = build_perceptron(output_perceptron_sizes)
+
+    @staticmethod
+    def _get_masked_mean_embeddings(embeddings: torch.Tensor, input_mask: torch.LongTensor):
+        input_mask = input_mask.unsqueeze(-1) # [batch size, seq_length, 1]
+
+        masked_embeddings = embeddings * input_mask
+
+        return torch.sum(masked_embeddings, dim=1) / torch.sum(input_mask, dim=1)
+    
+    @staticmethod
+    def _get_masked_max_embeddings(embeddings: torch.Tensor, input_mask: torch.LongTensor):
+        input_mask = input_mask.unsqueeze(-1) # [batch size, seq_length]
+
+        masked_embeddings = torch.where(input_mask.bool(), embeddings, -1e9)
+
+        return torch.max(masked_embeddings, dim=1)[0]
+
+    def forward(self, model_outputs: BaseModelOutput, **inputs: dict[str, torch.Tensor]):
+        # process CLS embeddings separately
+        cls_embeddings = model_outputs.last_hidden_state[:, 1]
+
+        other_embeddings = model_outputs.last_hidden_state[:, 1:]
+        input_mask = inputs["attention_mask"][:, 1:]
+
+        mean_embeddings = self._get_masked_mean_embeddings(other_embeddings, input_mask)
+        max_embeddings = self._get_masked_max_embeddings(other_embeddings, input_mask)
+
+        # reduce the size of pooled embeddings
+
+        mean_embeddings = self.down_size_linear_layers["mean"](mean_embeddings)
+        max_embeddings = self.down_size_linear_layers["max"](max_embeddings)
+
+        # concat all result into one tensor
+
+        all_features = torch.cat([cls_embeddings, mean_embeddings, max_embeddings], dim=1)
+
+        return self.classifier(all_features)
